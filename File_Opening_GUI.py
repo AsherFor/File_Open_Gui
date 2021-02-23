b09b9baa2f419f8d71ca62e3e359b0e9ca04b585
asherforman <asherforman@stu.postoakschool.org>
2021-02-23 10:05:53 -0600

    Commit

--- File_Opening_GUI.py
+++ File_Opening_GUI.py
@@ -1,14 +1,18 @@
-import tkinter
+'''
+Asher Forman
+File Opening GUI
+2/22/2021
+'''
 from tkinter import *
 from tkinter import filedialog
 import tkinter.scrolledtext as st
 
 master = Tk()
-master.title('A Random GUI')
+master.title('My File Opener')
 master.geometry("500x500")
-master.configure(background="grey30")
+master.configure(background="blue")
 
-# THIS IS A DROPDOWN SUBMENU EXAMPLE
+# Dropdown Submenu Example
 def closefile():
     print('Closing File')
 
@@ -16,7 +20,7 @@ def closefile():
 def openfile():
     print('Opening File')
     openfilename = filedialog.askopenfilename(title="Please select a file to open...", filetypes=(("txt files", "*.txt"),("all files", "*.*")))
-    print('Opening file at'+openfilename)
+    print('Opening file at'+ openfilename)
 
 def submenu():
     menu = Menu(master)
@@ -27,6 +31,24 @@ def submenu():
     submenu.add_separator()
     submenu.add_command(label="Close", command=closefile)
 
-submenu()
+def show_text_box():
+    print("show text box")
+    text_box = st.ScrolledText(master, bg = "grey20", font = 'TkFixedFont')
+    text_box.insert(INSERT, "inserting")
+    text_box.place(x = 100, y = 100, width = 100, height = 100)
 
+def read_file():
+    print("read file")
+
+def write_file():
+    print("write file")
+
+
+# Simple Buttons for Interface
+Button(master, text="Show Text Box", command = show_text_box, bg = "grey20", highlightbackground = "black", foreground = "blue").grid(row = 0, column = 0)
+Button(master, text="Get Text", command = read_file, bg = "grey20", highlightbackground = "black", foreground = "blue").grid(row = 0, column = 1)
+Button(master, text="Write Text", command = write_file, bg = "grey20", highlightbackground = "black", foreground = "blue").grid(row = 0, column = 2)
+
+
+submenu()
 master.mainloop()
